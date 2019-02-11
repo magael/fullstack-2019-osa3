2e9d52b1b852ffc8bf0be1fae85cd2423a9a7cc4
magael <mikael.jaakkola@helsinki.fi>
2019-02-11 23:40:31 +0200

    3.11

---
+++ build/precache-manifest.84d926f97c1a43befc70bfa74b2f5a28.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "093a9bd328aff0785b0f",
+    "url": "/static/js/main.093a9bd3.chunk.js"
+  },
+  {
+    "revision": "1bdeb50c48bfe976fdd4",
+    "url": "/static/js/1.1bdeb50c.chunk.js"
+  },
+  {
+    "revision": "4ae0f34781c8e2879402f0c9ab5505f3",
+    "url": "/index.html"
+  }
+];
